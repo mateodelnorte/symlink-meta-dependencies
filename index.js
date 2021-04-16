48d1a223bc01ae551e6a86cc15de9016957e0877
Patrick Lee Scott <pat@patscott.io>
2021-04-15 21:52:25 -0500

    fix: lint
    Signed-off-by: Patrick Lee Scott <pat@patscott.io>

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const debug = require('debug')('meta-exec');
 const execSync = require('child_process').execSync;
 const path = require('path');
 
-module.exports = function (options, cb) {
-
+module.exports = function(options, cb) {
   if (options.suppressLogging === undefined) options.suppressLogging = false;
 
   options.dir = options.dir || process.cwd();
@@ -16,41 +15,34 @@ module.exports = function (options, cb) {
   var code = null;
 
   try {
-
-    if ( ! options.suppressLogging) console.log(`\n${chalk.cyan(path.basename(options.displayDir))}:`)
+    if (!options.suppressLogging) console.log(`\n${chalk.cyan(path.basename(options.displayDir))}:`);
 
     code = execSync(options.cmd, { cwd: options.dir, env: process.env, stdio: 'inherit' });
-
   } catch (err) {
-    
     // a cmd error isnt a loop error,
     // we just want to output the cmd's output
     let errorMessage = `${chalk.red(path.basename(options.displayDir))} exited with error: ${err.toString()}`;
-    
-    if ( ! options.suppressLogging) console.error(errorMessage);
-    
+
+    if (!options.suppressLogging) console.error(errorMessage);
+
     if (cb) return cb(null, { error: errorMessage });
 
     return;
-
   }
 
   if (code) {
-    
     let errorMessage = `${chalk.red(path.basename(options.displayDir))} exited with code: ${code}`;
-    
-    if ( ! options.suppressLogging) console.error(errorMessage);
-    
+
+    if (!options.suppressLogging) console.error(errorMessage);
+
     if (cb) return cb(null, { err: errorMessage });
 
     return;
-
-  } 
+  }
 
   let success = chalk.green(`${path.basename(options.displayDir)} ✓`);
 
-  if ( ! options.suppressLogging) console.log(success);
-  
-  if (cb) return cb(null, { output: success });
+  if (!options.suppressLogging) console.log(success);
 
+  if (cb) return cb(null, { output: success });
 };
